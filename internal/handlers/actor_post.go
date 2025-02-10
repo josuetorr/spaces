69fc08078fc91e44b7bfdbf57dbe14f00a4f9d2a
josuetorr <josue.dtorres@proton.me>
2025-02-10 18:45:20 -0500

    moved actor creation logic to service from handler

--- internal/handlers/actor_post.go
+++ internal/handlers/actor_post.go
@@ -2,17 +2,14 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"log/slog"
 	"net/http"
-	"os"
 
-	"gitlab.com/josuetorr/spaces/internal/models"
 	"gitlab.com/josuetorr/spaces/internal/services"
 )
 
 type ActorService interface {
-	Create(a *models.Actor) error
+	Create(a services.CreateActorData) error
 }
 
 type PostActorHandler struct {
@@ -20,17 +17,12 @@ type PostActorHandler struct {
 	actorService ActorService
 }
 
-type createActorRequest struct {
-	username string
-	email    string
-}
-
 func NewPostActorHandler(log *slog.Logger, actorService services.ActorService) *PostActorHandler {
 	return &PostActorHandler{log: log, actorService: actorService}
 }
 
 func (h *PostActorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	var data createActorRequest
+	var data services.CreateActorData
 	defer r.Body.Close()
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
 		h.log.Error(err.Error())
@@ -38,18 +30,7 @@ func (h *PostActorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	domain := os.Getenv("domain")
-	actor := models.Actor{
-		Id:        fmt.Sprintf("%s/%s", domain, data.username),
-		Type:      models.Person,
-		Inbox:     fmt.Sprintf("%s/inbox", domain),
-		Outbox:    fmt.Sprintf("%s/outbox", domain),
-		Following: fmt.Sprintf("%s/following", domain),
-		Followers: fmt.Sprintf("%s/followers", domain),
-		Liked:     fmt.Sprintf("%s/liked", domain),
-	}
-
-	if err := h.actorService.Create(&actor); err != nil {
+	if err := h.actorService.Create(data); err != nil {
 		h.log.Error(err.Error())
 		http.Error(w, "Internal error", http.StatusInternalServerError)
 		return
--- internal/services/actor.go
+++ internal/services/actor.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"fmt"
+	"os"
+
 	"gitlab.com/josuetorr/spaces/internal/data"
 	"gitlab.com/josuetorr/spaces/internal/models"
 )
@@ -13,9 +16,26 @@ func NewActorService(repo data.ActorRepo) ActorService {
 	return ActorService{repo: repo}
 }
 
-func (s ActorService) Create(a *models.Actor) error {
-	if err := s.repo.Create(*a); err != nil {
+func (s ActorService) Create(data CreateActorData) error {
+	domain := os.Getenv("domain")
+	a := models.Actor{
+		Id:        fmt.Sprintf("%s/%s", domain, data.Username),
+		Type:      models.Person,
+		Inbox:     fmt.Sprintf("%s/inbox", domain),
+		Outbox:    fmt.Sprintf("%s/outbox", domain),
+		Following: fmt.Sprintf("%s/following", domain),
+		Followers: fmt.Sprintf("%s/followers", domain),
+		Liked:     fmt.Sprintf("%s/liked", domain),
+	}
+
+	if err := s.repo.Create(a); err != nil {
 		return err
 	}
 	return nil
+}
+
+type CreateActorData struct {
+	Username string
+	Email    string
+	Type     models.ActorType
 }
