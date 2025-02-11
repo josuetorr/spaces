57a6bda0950c6ff15c62494bc766c17e4b3f9963
josuetorr <josue.dtorres@proton.me>
2025-02-10 22:37:06 -0500

    proper response when fetching actor

--- internal/handlers/actor_get.go
+++ internal/handlers/actor_get.go
@@ -1,14 +1,30 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"github.com/go-chi/chi"
 )
 
-type GetActorHandler struct{}
+type GetActorHandler struct {
+	actorService ActorService
+}
 
-func NewGetActorHandler() *GetActorHandler {
-	return &GetActorHandler{}
+func NewGetActorHandler(ActorService ActorService) *GetActorHandler {
+	return &GetActorHandler{actorService: ActorService}
 }
 
 func (h *GetActorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	username := chi.URLParam(r, "username")
+
+	a, _ := h.actorService.Get(username)
+
+	if a == nil {
+		http.Error(w, "Resource not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/activity+json")
+	json.NewEncoder(w).Encode(a.ToDto())
 }
--- internal/models/actor.go
+++ internal/models/actor.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"fmt"
+	"os"
+)
+
 type ActorType string
 
 const (
@@ -15,4 +20,32 @@ type Actor struct {
 	Type              ActorType `json:"type"`
 	PreferredUsername string    `json:"preferredUsername"`
 	Email             string    `json:"email"`
+}
+
+type ActorDto struct {
+	Actor
+	Context   string `json:"@context"`
+	Inbox     string `json:"inbox"`
+	Outbox    string `json:"outbox"`
+	Following string `json:"following"`
+	Followers string `json:"followers"`
+	Liked     string `json:"liked"`
+}
+
+func (a Actor) ToDto() ActorDto {
+	enpoint := fmt.Sprintf("https://%s/%s", os.Getenv("SERVER_NAME"), a.Id)
+	return ActorDto{
+		Context: "https://www.w3.org/ns/activitystreams",
+		Actor: Actor{
+			Id:                a.Id,
+			Email:             a.Email,
+			PreferredUsername: a.PreferredUsername,
+			Type:              a.Type,
+		},
+		Inbox:     enpoint + "/inbox",
+		Outbox:    enpoint + "/outbox",
+		Following: enpoint + "/following",
+		Followers: enpoint + "/followers",
+		Liked:     enpoint + "/liked",
+	}
 }
